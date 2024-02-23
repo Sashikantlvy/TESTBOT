1d2121c123dc11a99efebec09027142d489f7824
Sashikantlvy <153577889+Sashikantlvy@users.noreply.github.com>
2024-02-23 10:57:14 +0530

    Update start.py
    Signed-off-by: Sashikantlvy <153577889+Sashikantlvy@users.noreply.github.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -26,7 +26,7 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_2"], callback_data="lood"),
             InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source"),
             
         ],
