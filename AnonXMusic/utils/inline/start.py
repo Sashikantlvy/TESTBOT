a21bab0effade4938afcd6a42e8f18365de6df26
Sashikantlvy <153577889+Sashikantlvy@users.noreply.github.com>
2024-02-24 17:09:44 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -33,5 +33,8 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
+        [
+            InlineKeyboardButton("ᴅᴏɴᴀᴛᴇ", callback_data="donate"),
+        ],
     ]
     return buttons
